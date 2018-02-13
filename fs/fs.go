51c99baa8d43e110d7d521539988fd1d266b9b39
Thomas <thomas68allison@hotmail.com>
2018-02-13 16:08:26 +0100

    First file server

--- fs/fs.go
+++ fs/fs.go
@@ -1,1 +1,18 @@
-package fs
+package main
+
+import (
+	"net/http"
+	"os"
+	"fmt"
+)
+
+func main() {
+	dir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	http.ListenAndServe(":3000", http.FileServer(http.Dir(dir)))
+}
